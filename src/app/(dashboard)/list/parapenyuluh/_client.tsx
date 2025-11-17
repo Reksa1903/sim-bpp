17700241f09689b66a53ab4aa2d0cf0690107111
Jarmyko <jarmyko98@gmail.com>
2025-11-17 20:16:45 +0700

    feat: kerjakan tugas teman 59

--- src/app/(dashboard)/list/parapenyuluh/_client.tsx
+++ src/app/(dashboard)/list/parapenyuluh/_client.tsx
@@ -77,7 +77,7 @@ const ParaPenyuluhClient = ({ data, count, role }: Props) => {
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">Semua Penyuluh</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-          <TableSearch value="" onChange={() => { }} />
+          <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-BppYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
