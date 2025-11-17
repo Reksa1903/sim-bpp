ac2742cb3ca347f63df2ad7905fffeb858a70a17
Jarmyko <jarmyko98@gmail.com>
2025-11-17 20:11:08 +0700

    feat: kerjakan tugas teman 58

--- src/app/(dashboard)/list/parapenyuluh/_client.tsx
+++ src/app/(dashboard)/list/parapenyuluh/_client.tsx
@@ -28,8 +28,6 @@ const ParaPenyuluhClient = ({ data, count, role }: Props) => {
     { header: 'Aksi', accessor: '' },
   ];
 
-  const [search, setSearch] = useState("");
-
   const renderRow = (item: PenyuluhWithDesa) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-BppLightBlue">
       <td className="flex items-center gap-4 p-4">
@@ -79,10 +77,7 @@ const ParaPenyuluhClient = ({ data, count, role }: Props) => {
       <div className="flex items-center justify-between">
         <h1 className="hidden md:block text-lg font-semibold">Semua Penyuluh</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
-          <TableSearch
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
+          <TableSearch value="" onChange={() => { }} />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-BppYellow">
               <Image src="/filter.png" alt="" width={14} height={14} />
